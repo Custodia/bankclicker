3adac04643b47433db1a6f174bfc46bd0b012bce
Olli Karppinen <olli.karppinen@relex.fi>
2017-11-24 21:54:56 +0200

    Adds legs and snout for pig

--- src/Pig.js
+++ src/Pig.js
@@ -21,7 +21,15 @@ class Pig {
     const context = state.context;
     context.save();
     context.fillStyle = '#f99';
-    context.fillRect(100, 200 - this.height, 150, 100);
+    const xOffset = state.screenWidth / 2 - 150;
+    const yOffset = - this.height + state.screenHeight / 2 - 200;
+    context.fillRect(100 + xOffset, 200 + yOffset, 150, 100);
+    context.fillRect(100 + xOffset, 300 + yOffset, 10, 10);
+    context.fillRect(240 + xOffset, 300 + yOffset, 10, 10);
+    context.fillRect(250 + xOffset, 230 + yOffset, 10, 40);
+    context.fillRect(90 + xOffset, 250 + yOffset, 10, 10);
+    context.fillStyle = '#000';
+    context.fillRect(220 + xOffset, 230 + yOffset, 10, 10);
     context.restore();
   }
 }
